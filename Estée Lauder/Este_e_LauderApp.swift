eaa7199a2034d9f2f8c1aff5748dc22ff1c77077
hande-nk <handenazkavas@gmail.com>
2023-08-11 21:07:52 +0300

    Initial Commit

---
+++ Estée Lauder/Este_e_LauderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Este_e_LauderApp.swift
+//  Estée Lauder
+//
+//  Created by Hande Naz Kavas on 11.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct Este_e_LauderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
